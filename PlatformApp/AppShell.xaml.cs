dbc1eea9b66ce8fb55963ee355079de97fc9d44a
ongzhixian <zhixian@hotmail.com>
2025-10-27 22:21:23 +0800

    Initial commit

---
+++ PlatformApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PlatformApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PlatformApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PlatformApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PlatformApp.Pages")]
